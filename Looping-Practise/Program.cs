07c21ba1d0f3bef60b2510c60014d7975a0a2cea
Rajvir Kaur <Rajvir.Kaur@cantire.com>
2019-08-19 13:09:50 -0400

    practise-C#

---
+++ Looping-Practise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Looping_Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int selection = 0;
+
+            do
+            {
+                Console.WriteLine("_______________________________________________");
+                Console.WriteLine(" 1. first Choice");
+                Console.WriteLine("2. Second Choice");
+                Console.WriteLine("3. EXIT");
+                Console.WriteLine("________________________________________________");
+
+                selection = Convert.ToInt32(Console.ReadLine());
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("You chose first option");
+                        break;
+                    case 2:
+                        Console.WriteLine("you chose second option");
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect entry - Try Again!!");
+                        break;
+
+                }
+            } while (selection != 3);
+
+            Console.WriteLine();
+            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+
+        }
+
+    }
+}
